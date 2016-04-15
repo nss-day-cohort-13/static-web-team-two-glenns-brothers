03fe6a2cb2c122a4841e7aaf96d056e61063df39
Russell Ries <riesru@gmail.com>
2016-04-14 20:59:10 -0500

    add objects in array

---
+++ products.js
@@ -0,0 +1,59 @@
+
+var products [
+
+  {
+    name: "Baseball",
+    description: "Ball used for playing baseball",
+    price: "$3.99",
+    img: "images/baseball.png"
+  }
+
+  {
+    name: "Basketball",
+    description: "Ball used for playing basketball",
+    price: "$19.99",
+    img: "images/basketball.png"
+  }
+
+  {
+    name: "Bowling Ball",
+    description: "Ball used for bowling",
+    price: "$199.99",
+    img: "images/bowling.png"
+  }
+
+  {
+    name: "Crystal Ball",
+    description: "Ball used for telling the future",
+    price: "Priceless",
+    img: "images/crystal.png"
+  }
+
+  {
+    name: "Football",
+    description: "Ball used for playing Football",
+    price: "$14.99",
+    img: "images/football.png"
+  }
+
+  {
+    name: "Golf Ball",
+    description: "Ball used for playing golf",
+    price: "$9.99 (for 3)",
+    img: "images/golfball.png"
+  }
+
+  {
+    name: "Soccer Ball",
+    description: "Ball used for playing soccer",
+    price: "$49.99",
+    img: "images/soccer.png"
+  }
+
+  {
+    name: "Tennis Ball",
+    description: "Ball used for playing tennis",
+    price: "$9.99 (for 3)",
+    img: "images/tennis.png"
+  }
+]
